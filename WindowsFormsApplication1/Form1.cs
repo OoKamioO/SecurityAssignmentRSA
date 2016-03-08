cc39fff15a4f3ef335a09b169b4a2bb9d1264078
Trent <iluvgurlzwithglasses45@gmail.com>
2016-03-08 12:42:47 +0000

    Added encryption key and bug checks

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -20,19 +20,62 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
         private void SubmitButton_Click(object sender, EventArgs e)
         {
-            if (Int32.TryParse(textBox1.Text, out prime1))
-            {
-                Prime checkPrime = new Prime(prime1);
+            int encryptKey = 0;
 
-                if (checkPrime.calculateIfPrime())
+            if (Int32.TryParse(textBox1.Text, out prime1) && Int32.TryParse(textBox2.Text, out prime2))
+            {
+                if (prime1 != prime2)
                 {
-                    primeText.Text = "Is an Prime Number";
+                    Prime checkPrime1 = new Prime(prime1);
+                    Prime checkPrime2 = new Prime(prime2);
+
+                    if (checkPrime1.calculateIfPrime() && checkPrime2.calculateIfPrime())
+                    {
+                        /*Numbers are prime*/
+                        int mod = (prime1 - 1) * (prime2 - 2); //Number of cooprimes in the product of prime1 and prime2
+
+                        for(int i = 2; i < mod; i++)
+                        {
+                            if((i%prime1 != 0) && (i%prime2 != 0) && (i%mod != 0))
+                            {
+                                encryptKey = i;
+                            }
+                        }
+
+                        primeText.Text = encryptKey + " is the encrypt key";
+
+                    }
+                    else
+                    {
+                        /*Numbers are even*/
+                        primeText.Text = "Please enter prime numbers";
+                    }
                 }
                 else
                 {
-                    primeText.Text = "Is an Even Number";
+                    primeText.Text = "Please enter unique Numbers";
                 }
             }
             else
